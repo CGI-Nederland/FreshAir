c069f64560d4e93cff0be0001a6b0a8decb6daae
Maurice Koster <maurice.koster@ing.nl>
2014-05-06 16:43:57 +0200

    logfmt added

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var logfmt = require("logfmt");
 var app = express();
+
 app.use(
         "/", //the URL throught which you want to access to you static content
         express.static(__dirname) //where your static content is located in your filesystem
     );
+app.use(logfmt.requestLogger());
 
-app.listen(80); //the port you want to use
+var port = Number(process.env.PORT || 80);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+}); //the port you want to use
